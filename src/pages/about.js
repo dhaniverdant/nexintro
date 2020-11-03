2220c0f202bc94540ee3e76fe3578ed17362cf60
dhani <dhaniverdant23@gmail.com>
2020-11-03 09:33:45 +0700

    create index and about page

---
+++ src/pages/about.js
@@ -0,0 +1,12 @@
+import Layout from "components/Layout"
+import styles from 'sass/Index.module.scss'
+
+const About = () => (
+  <Layout>
+    <div>
+      <h1 className={styles.title}>About Next.js</h1>
+    </div>
+  </Layout>
+);
+
+export default About;
---
+++ src/pages/index.js
@@ -0,0 +1,19 @@
+import Layout from "components/Layout"
+import styles from 'sass/Index.module.scss'
+
+const Index = ({stars}) => (
+  <Layout>
+    <div>
+      <h1 className={styles.title}>Welcome to Next.js</h1>
+      <div>Next stars: {stars}</div>
+    </div>
+  </Layout>
+);
+
+Index.getInitialProps = async () => {
+  const res = await fetch('https://api.github.com/repos/vercel/next.js')
+  const json = await res.json()
+  return { stars: json.stargazers_count }
+}
+
+export default Index;
