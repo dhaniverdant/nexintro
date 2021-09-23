3bcd5eed0e1f9ca19edcaeccd21ed86e0b4e425f
dhani <dhaniverdant23@gmail.com>
2021-09-23 11:26:35 +0800

    read and sort markdown files then show on index

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,21 +2,54 @@
 import React from 'react';
 
 import Layout from 'components/Layout';
-import styles from 'sass/Index.module.scss';
+import getSortedPostsData from '../../lib/posts';
 
-const Index = ({ stars }) => (
-  <Layout>
-    <div>
-      <h1 className={styles.title}>Welcome to Next.js</h1>
-      <div>Next stars is : {stars}</div>
-    </div>
-  </Layout>
-);
+export async function getStaticProps() {
+  const allPostsData = getSortedPostsData();
+  return {
+    props: {
+      allPostsData,
+    },
+  };
+}
 
-Index.getInitialProps = async () => {
-  const res = await fetch('https://api.github.com/repos/vercel/next.js');
-  const json = await res.json();
-  return { stars: json.owner.id };
-};
+export default function Home({ allPostsData }) {
+  return (
+    <Layout home>
+      {/* Keep the existing code here */}
 
-export default Index;
+      {/* Add this <section> tag below the existing <section> tag */}
+      <section>
+        <h2>Blog</h2>
+        <ul>
+          {allPostsData.map(({ id, date, title }) => (
+            <li key={id}>
+              {title}
+              <br />
+              {id}
+              <br />
+              {date}
+            </li>
+          ))}
+        </ul>
+      </section>
+    </Layout>
+  );
+}
+
+// const Index = ({ stars }) => (
+//   <Layout>
+//     <div>
+//       <h1 className={styles.title}>Welcome to Next.js</h1>
+//       <div>Next stars is : {stars}</div>
+//     </div>
+//   </Layout>
+// );
+
+// Index.getInitialProps = async () => {
+//   const res = await fetch('https://api.github.com/repos/vercel/next.js');
+//   const json = await res.json();
+//   return { stars: json.owner.id };
+// };
+
+// export default Index;
